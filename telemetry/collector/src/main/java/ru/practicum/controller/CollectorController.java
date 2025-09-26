2947eac32b541024f798788d6021044af5b2e451
Rinat <polaroid264@yandex.ru>
2025-09-26 17:39:25 +0300

    collector in process

---
+++ telemetry/collector/src/main/java/ru/practicum/controller/CollectorController.java
@@ -0,0 +1,22 @@
+package ru.practicum.controller;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.practicum.events.sensor.SensorEvent;
+
+@RestController
+@RequestMapping("/events")
+@RequiredArgsConstructor
+@Slf4j
+public class CollectorController {
+
+    @PostMapping("/sensors")
+    public void collectSensorEvent(@Valid @RequestBody SensorEvent event) {
+        // ... реализация метода ...
+    }
+}

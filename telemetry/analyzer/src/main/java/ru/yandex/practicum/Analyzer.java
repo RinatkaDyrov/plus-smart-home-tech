397732f368a3f86f92a4e3c43261a01c123f3eef
Rinat <polaroid264@yandex.ru>
2025-11-19 11:09:28 +0300

    4-analyzer pre-finish_5 some tests were failed

--- telemetry/analyzer/src/main/java/ru/yandex/practicum/Analyzer.java
+++ telemetry/analyzer/src/main/java/ru/yandex/practicum/Analyzer.java
@@ -7,18 +7,33 @@ import org.springframework.context.ConfigurableApplicationContext;
 import ru.yandex.practicum.processor.HubEventProcessor;
 import ru.yandex.practicum.processor.SnapshotProcessor;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 @SpringBootApplication
 @ConfigurationPropertiesScan
 public class Analyzer {
+    private static final ExecutorService executor = Executors.newFixedThreadPool(2);
+
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(Analyzer.class, args);
 
-        final HubEventProcessor hubEventProcessor = context.getBean(HubEventProcessor.class);
+        HubEventProcessor hubEventProcessor = context.getBean(HubEventProcessor.class);
         SnapshotProcessor snapshotProcessor = context.getBean(SnapshotProcessor.class);
 
-        Thread hubEventsThread = new Thread(hubEventProcessor, "HubEventHandlerThread");
-        hubEventsThread.start();
+        executor.submit(hubEventProcessor);
+        executor.submit(snapshotProcessor);
 
-        snapshotProcessor.run();
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            executor.shutdown();
+            try {
+                if (!executor.awaitTermination(5, TimeUnit.SECONDS)) {
+                    executor.shutdownNow();
+                }
+            } catch (InterruptedException e) {
+                executor.shutdownNow();
+            }
+        }));
     }
 }

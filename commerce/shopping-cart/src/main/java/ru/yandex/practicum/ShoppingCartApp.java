d0b29b19c0d29cf194e78244283280d7c7b168ad
Rinat <polaroid264@yandex.ru>
2025-12-09 09:42:48 +0300

    shopping-store done shopping-cart done warehouse done postman tests were passed EnableFeignClients annotation fixed

--- commerce/shopping-cart/src/main/java/ru/yandex/practicum/ShoppingCartApp.java
+++ commerce/shopping-cart/src/main/java/ru/yandex/practicum/ShoppingCartApp.java
@@ -3,10 +3,9 @@ package ru.yandex.practicum;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import ru.yandex.practicum.feignClient.WarehouseClient;
 
 @SpringBootApplication
-@EnableFeignClients(clients = WarehouseClient.class)
+@EnableFeignClients(basePackages = "ru/yandex/practicum/feignClient")
 public class ShoppingCartApp {
     public static void main(String[] args) {
         SpringApplication.run(ShoppingCartApp.class, args);
